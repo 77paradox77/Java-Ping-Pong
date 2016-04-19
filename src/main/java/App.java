cbcc60c30c11579b05522c12d4dface5634e5d43
Reed Lambier <Rlambier46@gmail.com>
2016-04-18 17:57:39 -0700

    It will add some UI. Will not connect with Layout.vtl file

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/hello.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/favorite_photos", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/favorite_photos.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+}
--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,11 +1,38 @@
+import java.util.Map;
+import java.util.HashMap;
 import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class PingPong {
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
     
+      String number = request.queryParams("number");
+      Integer integerNumber = Integer.parseInt(number);
+
+      // PingPong myPingPong = new PingPong();
+      ArrayList<Object> results = isPingPong(integerNumber);
+
+      model.put("finalNumber", results);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
-  public ArrayList<Object> isPingPong(Integer number) {
+
+
+  public static ArrayList<Object> isPingPong(Integer number) {
     ArrayList<Object> myList = new ArrayList<Object>();
     for (Integer index = 1; index <= number; index ++){
       if (index % 15 == 0){
